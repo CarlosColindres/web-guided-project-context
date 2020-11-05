094253f50fe477d6db84d496a4daf662671674bf
Warren C. Longmire <warren.longmire@lambdaschool.com>
2020-11-05 13:49:10 -0500

    lecture work-in-progress

--- followAlong/src/components/FamilyTree.js
+++ followAlong/src/components/FamilyTree.js
@@ -14,13 +14,13 @@ export default function FamilyTree() {
       <h2>Parents</h2>
 
       {/* Pass data down as a prop */}
-      <Parents parents={family} />
+      <Parents />
 
       <div className="spacer" />
       <h2>Siblings</h2>
 
       {/* Siblings will consume data from Context using render props */}
-      <Siblings family={family} />
+      <Siblings />
     </section>
   );
 }
