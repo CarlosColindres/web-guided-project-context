283fbfe66e5ad1c11badf9aa15fcae078141b108
Warren C. Longmire <warren.longmire@lambdaschool.com>
2020-11-05 12:50:23 -0500

    lecture work-in-progress

--- review/src/reducer.js
+++ review/src/reducer.js
@@ -1,4 +1,8 @@
 
+export const setName = (title, first, last) {
+    return({type:SET_NAME, payload:{title, first, last}});
+}
+
 export const reducer = (state, action) => {
     switch(action.type) {
         case(SET_NAME):
