3ed13c3b83d47b1d1943e0aeba3697fa3c2a8277
Warren C. Longmire <warren.longmire@lambdaschool.com>
2020-11-05 13:05:26 -0500

    lecture work-in-progress

--- review/src/reducer.js
+++ review/src/reducer.js
@@ -2,7 +2,7 @@ const SET_NAME = "SET_NAME";
 const SET_LOCATION = "SET_LOCATION";
 
 export const setName = (title, first, last) => {
-    return({type:SET_NAME, payload:{title, first, last}});
+    return({type:SET_NAME, payload:{title:title, first:first, last:list}});
 }
 
 export const setLocation = (street, city, state) => {
