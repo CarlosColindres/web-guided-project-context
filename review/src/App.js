1ddac3f59ef5ba718d89efa936cb81ac3b03d468
Warren C. Longmire <warren.longmire@lambdaschool.com>
2020-11-05 12:31:31 -0500

    lecture work-in-progress

--- review/src/App.js
+++ review/src/App.js
@@ -11,7 +11,7 @@ const App = ()=> {
 
 const SubComponent1 = (props) => {
     const { person, setPerson } = props;
-    
+
     return(<div className="component">
         <h2>SUBCOMPONENT 1</h2>
         <SubComponent2 person={person} setPerson={setPerson}/>
@@ -19,13 +19,22 @@ const SubComponent1 = (props) => {
 }
 
 const SubComponent2 = (props) => {
-    const {person} = props;
+    const {person, setPerson} = props;
 
     return(<div className="component">
         <h2>SUBCOMPONENT 2</h2>
         <p>Name: {person.name.title} {person.name.first} {person.name.last}</p>
-        {/* <SubComponent3 person={person} setPerson={setPerson}/> */}
+        <SubComponent3 person={person} setPerson={setPerson}/>
     </div>);
 }
 
+const SubComponent3 = (props) => {
+    const {person, setPerson} = props;
+
+    return(<div className="component">
+        <h2>SUBCOMPONENT 2</h2>
+        <p>Name: {person.name.title} {person.name.first} {person.name.last}</p>
+        <SubComponent3 person={person} setPerson={setPerson}/>
+    </div>);
+}
 export default App;
