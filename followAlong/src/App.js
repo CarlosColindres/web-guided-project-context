525dd64592f0014bf58ec0ec4e288abd7e073168
Warren C. Longmire <warren.longmire@lambdaschool.com>
2020-11-05 13:40:33 -0500

    lecture work-in-progress

--- followAlong/src/App.js
+++ followAlong/src/App.js
@@ -12,21 +12,23 @@ export default function App() {
   const [activeFamily, setActiveFamily] = useState(families[0]);
 
   return (
-    <div className="App">
-      <section className="header">
-        <h1>Family Trees</h1>
-        {families.map(d => (
-          <button
-            className={`family-button ${d.familyName ===
-              activeFamily.familyName && "active"}`}
-            key={d.familyName}
-            onClick={() => setActiveFamily(d)}
-          >
-            {d.familyName}
-          </button>
-        ))}
-      </section>
-      {activeFamily && <FamilyTree family={activeFamily} />}
-    </div>
+    <FamilyContext.Provider>
+      <div className="App">
+        <section className="header">
+          <h1>Family Trees</h1>
+          {families.map(d => (
+            <button
+              className={`family-button ${d.familyName ===
+                activeFamily.familyName && "active"}`}
+              key={d.familyName}
+              onClick={() => setActiveFamily(d)}
+            >
+              {d.familyName}
+            </button>
+          ))}
+        </section>
+        {activeFamily && <FamilyTree family={activeFamily} />}
+      </div>
+    </FamilyContext.Provider>
   );
 }
