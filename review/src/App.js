38db76d1bcc2d9c269c30cec3c48a04ae4a1cef8
Warren C. Longmire <warren.longmire@lambdaschool.com>
2020-11-05 12:59:58 -0500

    lecture work-in-progress

--- review/src/App.js
+++ review/src/App.js
@@ -12,7 +12,7 @@ const App = ()=> {
     return(<div className="component">
         <h1>APP COMPONENT</h1>
         {/* <PersonContext.Provider value={[person, setPerson]}> */}
-        <PersonContext.Provider value={[person, setName, setLocation, dispatch, "walliyBurger"]}>
+        <PersonContext.Provider value={[person, setName, setLocation, dispatch]}>
             <SubComponent1/>
         </PersonContext.Provider>
     </div>);
